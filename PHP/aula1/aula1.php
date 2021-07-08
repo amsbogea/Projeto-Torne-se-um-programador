f3927432ce1c30a2eed737cc3adcf5eff37db3ba
Alex Miranda <alex_miranda@outlook.com.br>
2021-07-08 11:51:53 -0300

    subindo para o github

---
+++ PHP/aula1/aula1.php
@@ -0,0 +1,23 @@
+<?php 
+	// este é um exeplo de comentario de 1 linha
+
+	# este tambem é cometario de 1 linha
+
+	/*
+	este 
+	eh
+	um comentario de 
+	varias linhas
+	*/
+
+	//echo "Minha aula de PHP";
+
+	# toda variavel começa com "$" seguida do nome da variável
+	$nome = "Meu nome é Alex Miranda<br>";
+	echo $nome;
+
+	$saldo = 100;
+	$valorDoSaque = 25;
+	$valorAposSaque = $saldo - $valorDoSaque;
+	echo "saldo atual = "; echo $valorAposSaque;
+ ?>
---
+++ PHP/aula1/vetor-matriz.php
@@ -0,0 +1,32 @@
+<?php 
+
+echo "<prev>";
+// Array Versões anteriors PHP
+$vetorCarros = array(
+				"Fiat" 	=> "Palio",
+				"Gm" 	=> "Celta",
+				"Ford" 	=> "Ranger"
+				 );
+// echo $vetorCarros["Gm"];
+// print_r($vetorCarros); 
+
+# Array Versões PHP 7
+$vetorCarrosPhp7 = ["Palio", "Celta", "Ranger"];
+
+#### Matriz
+
+$matriz2x2 = [
+				["9", "6"],
+				["8", "4"]
+			];
+
+$matriz3x3 = [
+				["10", "40", "60"],
+				["25", "90", "100"],
+				["15", "200","30"]
+			];
+#var_dump($matriz3x3);
+print_r($matriz3x3);
+
+
+ ?>
